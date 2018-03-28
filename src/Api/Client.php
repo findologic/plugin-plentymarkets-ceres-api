014c9507ace4a5e6a15d968f93de866603881e07
Tomas Semaška <tomas.semaska@adeoweb.lt>
2018-03-28 17:47:39 +0300

    Use http client class instead of using curl directly

--- src/Api/Client.php
+++ src/Api/Client.php
@@ -5,6 +5,7 @@ namespace Findologic\PluginPlentymarketsApi\Api;
 use Findologic\PluginPlentymarketsApi\Constants\Plugin;
 use Findologic\PluginPlentymarketsApi\Api\Request\Request;
 use Plenty\Plugin\Log\LoggerFactory;
+use HTTP_Request2;
 
 /**
  * Class Client
@@ -34,20 +35,10 @@ class Client
     {
         $response = false;
 
-        $connectionTimeOut = $request->getConfiguration(Plugin::API_CONFIGURATION_KEY_CONNECTION_TIME_OUT) ?? self::DEFAULT_CONNECTION_TIME_OUT;
-        $timeOut = $request->getConfiguration(Plugin::API_CONFIGURATION_KEY_TIME_OUT) ?? self::DEFAULT_CONNECTION_TIME_OUT;
-
         try {
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-            curl_setopt($ch, CURLOPT_URL, $request->getRequestUrl());
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
-            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $connectionTimeOut);
-            curl_setopt($ch, CURLOPT_TIMEOUT, $timeOut);
-            curl_setopt($ch, CURLOPT_POST, true);
-            $response = curl_exec($ch);
-            curl_close($ch);
+            $httpRequest = $this->createHttpRequest($request);
+            $response = $httpRequest->send();
+            $response = $response->getBody();
         } catch (\Exception $e) {
             $this->logger->warning('Exception while handling search query.');
             $this->logger->logException($e);
@@ -55,5 +46,16 @@ class Client
         }
 
         return $response;
+    }
+
+    public function createHttpRequest(Request $request)
+    {
+        $httpRequest =  new HTTP_Request2($request->getRequestUrl());
+        $httpRequest->setAdapter('curl');
+
+        $httpRequest->setConfig('connect_timeout', $request->getConfiguration(Plugin::API_CONFIGURATION_KEY_CONNECTION_TIME_OUT) ?? self::DEFAULT_CONNECTION_TIME_OUT);
+        $httpRequest->setConfig('timeout',$request->getConfiguration(Plugin::API_CONFIGURATION_KEY_TIME_OUT) ?? self::DEFAULT_CONNECTION_TIME_OUT);
+
+        return $httpRequest;
     }
 }
