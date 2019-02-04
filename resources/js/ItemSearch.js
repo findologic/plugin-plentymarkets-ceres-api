3430437ea00bc45c70c5bee9e22c74c77ab1d23e
Ismail Hanli <i.hanli@findologic.com>
2019-02-04 10:42:28 +0100

    Remove unnecessary event handlers

--- resources/js/ItemSearch.js
+++ resources/js/ItemSearch.js
@@ -1,92 +1,46 @@
-import Url from "./mixins/url";
-
 Vue.component("item-search", {
-    mixins: [Url],
-    delimiters: ["${", "}"],
-    props: [
-        "template"
-    ],
-
-    data()
-    {
-        return {
-            isSearchFocused: false
-        };
-    },
-
     computed:
+    {
+        selectedAutocompleteItem()
         {
-            selectedAutocompleteItem()
-            {
-                return null;
-            }
+            return null;
+        }
+    },
+    methods:
+    {
+        prepareSearch()
+        {
+            this.search();
+
+            $("#searchBox").collapse("hide");
         },
 
-    created()
-    {
-        this.$options.template = this.template;
-    },
-
-    mounted()
-    {
-        this.$nextTick(() =>
+        search()
         {
-            const urlParams = Url.getUrlParams(document.location.search);
+            let searchBaseURL = "/search?query=";
 
-            this.$store.commit("setItemListSearchString", urlParams.query);
-
-            if (urlParams === null || typeof urlParams === 'undefined') {
-                this.$refs.searchInput.value = '';
-            } else {
-                this.$refs.searchInput.value = urlParams.query;
+            if (App.defaultLanguage !== App.language)
+            {
+                searchBaseURL = `/${App.language}/search?query=`;
             }
-        });
-    },
 
-    methods:
+            window.open(searchBaseURL + this.$refs.searchInput.value, "_self", false);
+        },
+
+        autocomplete(searchString)
         {
-            prepareSearch()
-            {
-                this.search();
+        },
 
-                $("#searchBox").collapse("hide");
-            },
+        selectAutocompleteItem(item)
+        {
+        },
 
-            search()
-            {
-                let searchBaseURL = "/search?query=";
+        keyup()
+        {
+        },
 
-                if (App.defaultLanguage !== App.language)
-                {
-                    searchBaseURL = `/${App.language}/search?query=`;
-                }
-
-                window.open(searchBaseURL + this.$refs.searchInput.value, "_self", false);
-            },
-
-            autocomplete(searchString)
-            {
-            },
-
-            selectAutocompleteItem(item)
-            {
-            },
-
-            keyup()
-            {
-            },
-
-            keydown()
-            {
-            },
-
-            // hide autocomplete after 100ms to make clicking on it possible
-            setIsSearchFocused(value)
-            {
-                setTimeout(() =>
-                {
-                    this.isSearchFocused = !!value;
-                }, 100);
-            }
+        keydown()
+        {
         }
+    }
 });
