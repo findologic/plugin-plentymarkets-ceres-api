1450038303d04b693a5f9a7eac57dd05cca0edd2
Wladislaw Koschemako <w.koschemako@findologic.com>
2019-03-05 12:55:57 +0100

    Add Item search

--- resources/js/src/app/components/itemList/ItemSearch.js
+++ resources/js/src/app/components/itemList/ItemSearch.js
@@ -1,77 +1,77 @@
-// Vue.component("item-search", {
-//
-//     props: [
-//         'template'
-//     ],
-//
-//     data()
-//     {
-//         return {
-//             promiseCount: 0,
-//             autocompleteResult: [],
-//             selectedAutocompleteIndex: -1,
-//             isSearchFocused: false
-//         };
-//     },
-//
-//
-//     computed:
-//     {
-//         selectedAutocompleteItem()
-//         {
-//             return null;
-//         }
-//     },
-//
-//     created()
-//     {
-//         this.$options.template = this.template;
-//     },
-//
-//     methods:
-//     {
-//         prepareSearch()
-//         {
-//             this.search();
-//
-//             $('#searchBox').collapse('hide');
-//         },
-//
-//         search()
-//         {
-//             let searchBaseURL = '/search?query=';
-//
-//             if (App.defaultLanguage !== App.language)
-//             {
-//                 searchBaseURL = `/${App.language}/search?query=`;
-//             }
-//
-//             window.open(searchBaseURL + this.$refs.searchInput.value, '_self', false);
-//         },
-//
-//         autocomplete(searchString)
-//         {
-//         },
-//
-//         selectAutocompleteItem(item)
-//         {
-//         },
-//
-//         keyup()
-//         {
-//         },
-//
-//         keydown()
-//         {
-//         },
-//
-//         // hide autocomplete after 100ms to make clicking on it possible
-//         setIsSearchFocused(value)
-//         {
-//             setTimeout(() =>
-//             {
-//                 this.isSearchFocused = !!value;
-//             }, 100);
-//         }
-//     }
-// });
+Vue.component("item-search", {
+
+    props: [
+        'template'
+    ],
+
+    data()
+    {
+        return {
+            promiseCount: 0,
+            autocompleteResult: [],
+            selectedAutocompleteIndex: -1,
+            isSearchFocused: false
+        };
+    },
+
+
+    computed:
+    {
+        selectedAutocompleteItem()
+        {
+            return null;
+        }
+    },
+
+    created()
+    {
+        this.$options.template = this.template;
+    },
+
+    methods:
+    {
+        prepareSearch()
+        {
+            this.search();
+
+            $('#searchBox').collapse('hide');
+        },
+
+        search()
+        {
+            let searchBaseURL = '/search?query=';
+
+            if (App.defaultLanguage !== App.language)
+            {
+                searchBaseURL = `/${App.language}/search?query=`;
+            }
+
+            window.open(searchBaseURL + this.$refs.searchInput.value, '_self', false);
+        },
+
+        autocomplete(searchString)
+        {
+        },
+
+        selectAutocompleteItem(item)
+        {
+        },
+
+        keyup()
+        {
+        },
+
+        keydown()
+        {
+        },
+
+        // hide autocomplete after 100ms to make clicking on it possible
+        setIsSearchFocused(value)
+        {
+            setTimeout(() =>
+            {
+                this.isSearchFocused = !!value;
+            }, 100);
+        }
+    }
+});
