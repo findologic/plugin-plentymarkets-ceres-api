29e1a14a6fca22d96d76657627d57f561dc7d7d3
Ismail Hanli <i.hanli@findologic.com>
2019-09-09 17:30:10 +0200

    Remove debug logs

--- src/Api/Request/ParametersBuilder.php
+++ src/Api/Request/ParametersBuilder.php
@@ -70,7 +70,6 @@ class ParametersBuilder
 
         if (isset($parameters[Plugin::API_PARAMETER_ATTRIBUTES])) {
             $attributes = $parameters[Plugin::API_PARAMETER_ATTRIBUTES];
-            $this->logger->error('attribs', $attributes);
             foreach ($attributes as $key => $value) {
                 if ($key === 'cat' && $category) {
                     continue;
