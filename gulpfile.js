a2bd0ddddb1c4df5ff32afcc02b447bc05b94255
darius-adeoweb <46957878+darius-adeoweb@users.noreply.github.com>
2019-09-09 10:36:43 +0300

    Plenty 129 add support for filter type dropdown (#81)
    * PLENTY-129 Add support for filter type dropdown
    
    * Dropdown styling (#83)
    
    * Lay ground work
    
    * Dropdown effect
    
    * Further styling
    
    * Further styling
    
    * Remove CSS class from slider container
    
    * Further styling
    
    * Fix padding
    
    * Fix alignment
    
    * Fix alignment
    
    * Add padding
    
    * Update dropdown interaction
    
    * Make dropdown options selectable
    
    * Close dropdown on blur
    
    * Add compiled files
    
    * Fix typo in function name
    
    * Use native JS event
    
    * Mark div as editable
    
    * Assign tab index
    
    * Fix slider alignment
    
    * Fix dropdown alignment
    
    * Additional styles
    
    * Remove dead code

--- gulpfile.js
+++ gulpfile.js
@@ -21,6 +21,7 @@ gulp.task("js", () => {
             "app/components/itemList/filter/ItemFilterTagList.js",
             "app/components/itemList/filter/ItemRangeSlider.js",
             "app/components/itemList/filter/ItemColorTiles.js",
+            "app/components/itemList/filter/ItemDropdown.js",
             "app/components/itemList/ItemListSorting.js",
             "app/components/itemList/ItemsPerPage.js",
             "app/components/itemList/Pagination.js",
