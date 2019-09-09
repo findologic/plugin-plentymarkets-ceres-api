01df3feb8bca945aa80edccae3cb6d365d0eab0e
Ismail Hanli <i.hanli@findologic.com>
2019-09-09 17:36:05 +0200

    Log search params

--- src/Api/Request/ParametersBuilder.php
+++ src/Api/Request/ParametersBuilder.php
@@ -65,7 +65,7 @@ class ParametersBuilder
     ) {
         $parameters = (array) $httpRequest->all();
 
-        $this->logger->error('all params', $httpRequest->all());
+        $this->logger->error('all params (casted)', $parameters);
 
         $request->setParam('query', $externalSearch->searchString);
         $request->setPropertyParam(Plugin::API_PROPERTY_VARIATION_ID);
