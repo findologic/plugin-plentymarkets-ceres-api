7d637668539a944d36512b96438ed1200c1b6931
Ismail Hanli <i.hanli@findologic.com>
2019-09-09 17:21:16 +0200

    Log search params

--- src/Api/Request/ParametersBuilder.php
+++ src/Api/Request/ParametersBuilder.php
@@ -70,14 +70,13 @@ class ParametersBuilder
 
         if (isset($parameters[Plugin::API_PARAMETER_ATTRIBUTES])) {
             $attributes = $parameters[Plugin::API_PARAMETER_ATTRIBUTES];
+            $this->logger->error('attribs', $attributes);
             foreach ($attributes as $key => $value) {
                 if ($key === 'cat' && $category) {
                     continue;
                 }
 
                 $request->setAttributeParam($key, $value);
-
-                $this->logger->error($key, $value);
             }
         }
 
