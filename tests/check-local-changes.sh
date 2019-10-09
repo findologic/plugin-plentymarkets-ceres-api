7199f51fe641d22d9501d1e0a1ab8ef227a8b35b
Dominik Brader <d.brader@findologic.com>
2019-10-09 11:33:28 +0200

    Call npm via travis.yml

--- tests/check-local-changes.sh
+++ tests/check-local-changes.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-npm run build
-
 if [[ `git status --porcelain` ]]; then
-  echo 'Local changes found. Ensure to build your JS and CSS files by running "gulp".'
+  echo 'Local changes found. Ensure to build your JS and CSS files by running "gulp" or "npm run build".'
   exit 1
 else
   echo 'No local changes found, JS and CSS files were compiled properly.'
