66041b51f5dcfcc4bd808e498d4d5d32d85821ee
Dominik Brader <d.brader@findologic.com>
2019-10-09 10:58:08 +0200

    Test

--- tests/check-local-changes.sh
+++ tests/check-local-changes.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-
-VN=$(git describe --abbrev=7 HEAD 2>/dev/null)
-
-git update-index -q --refresh
-CHANGED=$(git diff-index --name-only HEAD --)
-if [[ -n "$CHANGED" ]]; then
-  echo 'No local changes found, JS and CSS files were compiled properly.'
-  exit 0
-else
-  echo 'Local changes found. Ensure to build your JS and CSS files by running "gulp".'
-  exit 1
-fi
+#
+#VN=$(git describe --abbrev=7 HEAD 2>/dev/null)
+#
+#git update-index -q --refresh
+#CHANGED=$(git diff-index --name-only HEAD --)
+#if [[ -n "$CHANGED" ]]; then
+#  echo 'No local changes found, JS and CSS files were compiled properly.'
+#  exit 0
+#else
+#  echo 'Local changes found. Ensure to build your JS and CSS files by running "gulp".'
+#  exit 1
+#fi
 
 
 
