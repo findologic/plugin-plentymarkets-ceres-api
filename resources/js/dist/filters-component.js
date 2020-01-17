76339e470a8c879370f49002d254dde74100d226
Dominik Brader <d.brader@findologic.com>
2020-01-17 10:59:21 +0100

    Fix approach

--- resources/js/dist/filters-component.js
+++ resources/js/dist/filters-component.js
@@ -743,7 +743,7 @@ exports.default = {
             for (i = 0; i < sal; i++) {
                 tmp = strArr[i].split('=');
                 key = fixStr(tmp[0]);
-                value = tmp.length < 2 ? '' : fixStr(tmp[1]).replace(/\+/g, '%20');
+                value = tmp.length < 2 ? '' : fixStr(tmp[1]).replace(/\+/g, ' ');
 
                 while (key.charAt(0) === ' ') {
                     key = key.slice(1);
