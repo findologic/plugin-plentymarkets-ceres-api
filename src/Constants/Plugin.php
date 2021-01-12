38f057d2ab1510c781ff85918cb568a27bd65557
Dominik Brader <d.brader@findologic.com>
2021-01-12 10:44:31 +0100

    PLENTY-287 change the way the search bar is overriden

--- src/Constants/Plugin.php
+++ src/Constants/Plugin.php
@@ -10,7 +10,7 @@ class Plugin
 {
     const PLUGIN_NAMESPACE = 'Findologic';
     const PLUGIN_IDENTIFIER = 'findologic-plugin-api';
-    const PLUGIN_VERSION = '3.2.2';
+    const PLUGIN_VERSION = '3.3.0';
 
     const PLENTY_PARAMETER_PAGINATION_ITEMS_PER_PAGE = 'items';
 
