feec3ecf5fbdcf1ddb17af88cea2e60156bbe24a
RimantasAdeoWeb <61787115+RimantasAdeoWeb@users.noreply.github.com>
2021-04-19 12:39:21 +0300

    PLENTY-324 fix default filters not working when an incorrect shopkey … (#253)
    * PLENTY-324 fix default filters not working when an incorrect shopkey is entered
    
    * PLENTY-324 remove log
    
    * Fix docblock
    
    * Add more middleware tests (#254)
    
    Co-authored-by: Rimantas Galvonas <rimantas@adeoweb.biz>
    Co-authored-by: Dominik Brader <d.brader@findologic.com>

--- src/Providers/FindologicServiceProvider.php
+++ src/Providers/FindologicServiceProvider.php
@@ -3,15 +3,10 @@
 namespace Findologic\Providers;
 
 use Findologic\Components\PluginConfig;
-use Findologic\Contexts\FindologicCategoryItemContext;
-use Findologic\Contexts\FindologicItemSearchContext;
 use Findologic\Services\SearchService;
 use Findologic\Validators\PluginConfigurationValidator;
-use IO\Helper\TemplateContainer;
-use Plenty\Plugin\Events\Dispatcher;
 use Plenty\Plugin\ServiceProvider;
 use Findologic\Middlewares\Middleware;
-use Plenty\Plugin\Templates\Twig;
 
 /**
  * Class FindologicServiceProvider
@@ -30,25 +25,6 @@ class FindologicServiceProvider extends ServiceProvider
         }
 
         $this->addGlobalMiddleware(Middleware::class);
-    }
-
-    public function boot(Twig $twig, Dispatcher $eventDispatcher)
-    {
-        if (!$this->validatePluginConfiguration()) {
-            return;
-        }
-
-        $eventDispatcher->listen('IO.ctx.search', function (TemplateContainer $templateContainer, $templateData = []) {
-            $templateContainer->setContext(FindologicItemSearchContext::class);
-            return false;
-        });
-        $eventDispatcher->listen(
-            'IO.ctx.category.item',
-            function (TemplateContainer $templateContainer, $templateData = []) {
-                $templateContainer->setContext(FindologicCategoryItemContext::class);
-                return false;
-            }
-        );
     }
 
     private function validatePluginConfiguration(): bool
