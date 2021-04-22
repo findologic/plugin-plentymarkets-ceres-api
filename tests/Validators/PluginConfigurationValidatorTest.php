fac55aa4b29735e93c5b0b963d14a5c911ceaafa
Dominik Brader <d.brader@findologic.com>
2021-04-22 11:42:53 +0200

    Remove PluginOrderValidator

--- tests/Validators/PluginConfigurationValidatorTest.php
+++ tests/Validators/PluginConfigurationValidatorTest.php
@@ -3,7 +3,6 @@
 namespace Findologic\Tests\Validators;
 
 use Findologic\Validators\PluginConfigurationValidator;
-use Findologic\Validators\PluginOrderValidator;
 use Findologic\Validators\ShopkeyValidator;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
@@ -11,11 +10,6 @@ use Plenty\Log\Contracts\LoggerContract;
 
 class PluginConfigurationValidatorTest extends TestCase
 {
-    /**
-     * @var PluginOrderValidator|MockObject
-     */
-    private $pluginOrderValidatorMock;
-
     /**
      * @var ShopkeyValidator|MockObject
      */
@@ -33,45 +27,28 @@ class PluginConfigurationValidatorTest extends TestCase
 
     public function setup()
     {
-        $this->pluginOrderValidatorMock = $this->getMockBuilder(PluginOrderValidator::class)
-            ->setMethods(['validate'])
-            ->getMock();
         $this->shopKeyValidatorMock = $this->getMockBuilder(ShopkeyValidator::class)
             ->disableOriginalConstructor()
             ->setMethods(['validate'])
             ->getMock();
         $this->mainValidatorMock = $this->getMockBuilder(PluginConfigurationValidator::class)
-            ->setMethods(['getPluginOrderValidator', 'getShopkeyValidator', 'getLoggerObject'])
+            ->setMethods(['getShopkeyValidator', 'getLoggerObject'])
             ->getMock();
         $this->loggerMock = $this->getMockForAbstractClass(LoggerContract::class);
 
-        $this->mainValidatorMock->method('getPluginOrderValidator')->willReturn($this->pluginOrderValidatorMock);
         $this->mainValidatorMock->method('getShopkeyValidator')->willReturn($this->shopKeyValidatorMock);
         $this->mainValidatorMock->method('getLoggerObject')->willReturn($this->loggerMock);
     }
 
     public function testItIsValidWhenBothSubvalidatorsAreValid()
     {
-        $this->pluginOrderValidatorMock->method('validate')->willReturn(true);
         $this->shopKeyValidatorMock->method('validate')->willReturn(true);
 
         $this->assertTrue($this->mainValidatorMock->validate());
     }
 
-    public function testItIsNotValidAndItDoesNotCheckShopKeyIfPluginOrderValidationFails()
-    {
-        $this->pluginOrderValidatorMock->method('validate')->willReturn(false);
-        $this->shopKeyValidatorMock->expects($this->never())->method('validate');
-        $this->loggerMock->expects($this->once())
-            ->method('error')
-            ->with('IO plugin must be loaded before the Findologic plugin. Check the plugin priorities.');
-
-        $this->assertFalse($this->mainValidatorMock->validate());
-    }
-
     public function testItIsNotValidIfShopkeyValidationFails()
     {
-        $this->pluginOrderValidatorMock->method('validate')->willReturn(true);
         $this->shopKeyValidatorMock->method('validate')->willReturn(false);
         $this->loggerMock->expects($this->once())
             ->method('notice')
@@ -82,7 +59,7 @@ class PluginConfigurationValidatorTest extends TestCase
 
     public function testItDoesNotRunValidatorsMoreThanOnce()
     {
-        $this->pluginOrderValidatorMock->expects($this->once())->method('validate');
+        $this->shopKeyValidatorMock->expects($this->once())->method('validate');
 
         $this->mainValidatorMock->validate();
         $this->mainValidatorMock->validate();
