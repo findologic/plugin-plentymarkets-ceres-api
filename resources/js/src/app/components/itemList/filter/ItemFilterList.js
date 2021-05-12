04dd0afb83dd584bb933d5937e36baedafac3b4b
Rimantas Galvonas <rimantas@adeoweb.biz>
2021-05-12 12:51:20 +0300

    PLENTY-337 add configuration for showing of selected filters count

---
+++ resources/js/src/app/components/itemList/filter/ItemFilterList.js
@@ -0,0 +1,35 @@
+Vue.component("findologic-item-filter-list", {
+    props: {
+        facetData: {
+            type: Array,
+            default: () => []
+        },
+        allowedFacetsTypes: {
+            type: Array,
+            default: () => []
+        },
+        showSelectedFiltersCount: {
+            type: Boolean,
+            default: false
+        }
+    },
+
+    computed: {
+        ...Vuex.mapState({
+            facets(state)
+            {
+                if (!this.allowedFacetsTypes.length) {
+                    return state.itemList.facets;
+                }
+
+                return state.itemList.facets.filter(facet => this.allowedFacetsTypes.includes(facet.id) || this.allowedFacetsTypes.includes(facet.type));
+            },
+            isLoading: state => state.itemList.isLoading,
+            selectedFacets: state => state.itemList.selectedFacets
+        })
+    },
+
+    created() {
+        this.$store.commit("addFacets", this.facetData);
+    }
+});
