8e56f24f9002cd640bbb7e5b1bfab8b96c263ca8
Dominik Brader <d.brader@findologic.com>
2021-10-27 16:06:39 +0200

    Bump version

--- src/Constants/Plugin.php
+++ src/Constants/Plugin.php
@@ -10,7 +10,7 @@ class Plugin
 {
     const PLUGIN_NAMESPACE = 'Findologic';
     const PLUGIN_IDENTIFIER = 'findologic-plugin-api';
-    const PLUGIN_VERSION = '3.7.2';
+    const PLUGIN_VERSION = '3.7.3';
 
     const PLENTY_PARAMETER_PAGINATION_ITEMS_PER_PAGE = 'items';
 
