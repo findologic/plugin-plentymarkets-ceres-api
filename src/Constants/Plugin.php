200d36edbe0fa77e7f20209336c819af1c9ee68d
Thomas Ballmann <mail@i-2xdream.de>
2022-07-11 15:43:21 +0200

    Update Plugin.php

--- src/Constants/Plugin.php
+++ src/Constants/Plugin.php
@@ -10,7 +10,7 @@ class Plugin
 {
     const PLUGIN_NAMESPACE = 'Findologic';
     const PLUGIN_IDENTIFIER = 'findologic-plugin-api';
-    const PLUGIN_VERSION = '4.0.4';
+    const PLUGIN_VERSION = '4.0.5';
 
     const PLENTY_PARAMETER_PAGINATION_ITEMS_PER_PAGE = 'items';
 
