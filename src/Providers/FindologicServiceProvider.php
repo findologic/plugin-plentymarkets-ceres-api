50f22337fd0d7c98d163f0c45fc0e22e2fd6ec56
mariusslo <marius.slovikas@gmail.com>
2023-02-13 11:02:01 +0200

    Fixed translations

--- src/Providers/FindologicServiceProvider.php
+++ src/Providers/FindologicServiceProvider.php
@@ -2,11 +2,14 @@
 
 namespace Findologic\Providers;
 
-use Findologic\Components\PluginConfig;
-use Findologic\Services\SearchService;
-use Findologic\Validators\PluginConfigurationValidator;
+use Ceres\Helper\LayoutContainer;
+use Plenty\Plugin\Templates\Twig;
 use Plenty\Plugin\ServiceProvider;
+use Plenty\Plugin\Events\Dispatcher;
 use Findologic\Middlewares\Middleware;
+use Findologic\Services\SearchService;
+use Findologic\Components\PluginConfig;
+use Findologic\Validators\PluginConfigurationValidator;
 
 /**
  * Class FindologicServiceProvider
@@ -19,7 +22,15 @@ class FindologicServiceProvider extends ServiceProvider
         $this->getApplication()->singleton(SearchService::class);
         $this->getApplication()->singleton(PluginConfig::class);
         $this->getApplication()->singleton(PluginConfigurationValidator::class);
+        // $this->getApplication()->register(FindologicTemplateProvider::class);
 
         $this->addGlobalMiddleware(Middleware::class);
+    }
+
+    public function boot(Twig $twig, Dispatcher $eventDispatcher,)
+    {
+        $eventDispatcher->listen("Ceres.LayoutContainer.Script.Loader", function(LayoutContainer $container) use ($twig) {
+            $container->addContent($twig->render("Findologic::content.translations"));
+        });
     }
 }
