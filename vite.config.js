599fa27fc9fb8b2440c20ce465769101cdf83775
mariusslo <marius.slovikas@gmail.com>
2023-03-28 15:57:57 +0300

    test

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,22 @@
 import { createVuePlugin as vue } from 'vite-plugin-vue2';
 import { defineConfig } from 'vite';
 import mkcert from 'vite-plugin-mkcert';
+import commonjs from '@rollup/plugin-commonjs';
+
 const path = require('path');
 
 export default defineConfig({
     server: { https: true },
-    plugins: [vue(), mkcert()],
+    plugins: [vue(), mkcert(), commonjs()],
     alias: {
         '@': path.resolve(__dirname, './'),
     },
     build: {
+        lib: {
+            entry: './resources/js/src/index.ts',
+            formats: ['cjs']
+        },
+        target: 'es2020',
         rollupOptions: {
             input: {
                 app: './resources/js/src/index.ts',
@@ -20,5 +27,9 @@ export default defineConfig({
                 assetFileNames: 'findologic_ceres.css',
             }
         },
+        commonjsOptions: {
+            transformMixedEsModules: true
+        }
+
     },
 });
