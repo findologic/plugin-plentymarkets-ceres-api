651e34ade2cd13d16771871b6f69821e3dd0cec2
rimvydas <rimvydas.levinskis@hashtages.com>
2023-10-23 11:46:37 +0300

    request log

--- resources/lib/findologic_client.php
+++ resources/lib/findologic_client.php
@@ -19,5 +19,5 @@ try {
     $response = new ApiResponse($findologicClient->send($request));
     return ['response'=> (array)$response];
 } catch (\Throwable $t) {
-    return ['error' => (string)$t];
+    return ['error' => (string)$t, 'request' => (array)$request];
 }
