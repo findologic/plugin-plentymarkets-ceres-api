9f8003a0f08cf41c97fe27953e0e1fc186995377
rimvydas <rimvydas.levinskis@hashtages.com>
2023-10-30 12:47:21 +0200

    response change arrays params

--- src/Api/Response/Response.php
+++ src/Api/Response/Response.php
@@ -15,8 +15,8 @@ class Response{
     public function __construct(array $response)
     {
         $this->getLogger(__METHOD__)->error('response from Response.class', $response);
-        $this->request = pluginApp(Request::class, $response['response']['request']);
-        $this->result = pluginApp(Result::class, $response['response']['result']);;
+        $this->request = pluginApp(Request::class, $response['request']);
+        $this->result = pluginApp(Result::class, $response['result']);;
     }
 
     /**
