a2486731ae1871e159d3b835b42621b3dc0155c8
rimvydas <rimvydas.levinskis@hashtages.com>
2023-10-30 16:32:36 +0200

    add name to landingpage

--- src/Struct/LandingPage.php
+++ src/Struct/LandingPage.php
@@ -5,8 +5,9 @@ namespace Findologic\Struct;
 class LandingPage
 {
     protected ?string $link;
+    protected ?string $name;
 
-    public function __construct(?string $link, string $name)
+    public function __construct(?string $link, ?string $name)
     {
         $this->link = $link;
     }
@@ -14,5 +15,10 @@ class LandingPage
     public function getLink(): ?string
     {
         return $this->link;
+    }
+
+    public function getName()
+    {
+        return $this->name;
     }
 }
