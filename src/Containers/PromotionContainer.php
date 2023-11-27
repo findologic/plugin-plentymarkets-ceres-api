6bca75bd8bd6006857c46436007244c539603526
rimvydas <rimvydas.levinskis@hashtages.com>
2023-11-27 09:33:16 +0200

    throw instead of log

--- src/Containers/PromotionContainer.php
+++ src/Containers/PromotionContainer.php
@@ -26,7 +26,7 @@ class PromotionContainer
         }
 
         $searchResults = $searchService->getResults();
-        $this->getLogger(__METHOD__)->error('promotion', $searchResults->getPromotionExtension());
+        throw new \Exception(json_encode($searchResults->getPromotionExtension()));
         return $twig->render(
             'Findologic::Category.Item.Partials.Promotion',
             [
