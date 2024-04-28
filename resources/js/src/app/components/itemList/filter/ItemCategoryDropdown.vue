91821f1da9f6bce3124be957cf2352143a5275fc
aserstobitov <artiom.serstobitov@hashtages.com>
2024-04-28 22:31:36 +0300

    console.log

--- resources/js/src/app/components/itemList/filter/ItemCategoryDropdown.vue
+++ resources/js/src/app/components/itemList/filter/ItemCategoryDropdown.vue
@@ -124,7 +124,7 @@ export default defineComponent({
       if (typeof props.currentCategory !== 'undefined' && isParentCategorySelected()) {
         return false;
       }
-      console.log(UrlBuilder.getSelectedFilters());
+      console.log(typeof UrlBuilder.getSelectedFilters().find(element => element.id === props.facet.id) !== 'undefined');
       return typeof UrlBuilder.getSelectedFilters().find(element => element.id === props.facet.id) !== 'undefined';
     });
 
